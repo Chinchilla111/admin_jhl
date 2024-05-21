8cda641e48f5bd0fc31b5d481de804e8c2bfe3cb
jhl <jhl@yundragon.com>
2024-05-21 09:40:42 +0800

    打包配置

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,//关闭严格代码格式，不按规定书写会报错项目启动不起来
+  publicPath:'/dist/',
 })
